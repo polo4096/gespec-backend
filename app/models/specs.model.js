72ba0df02b9f352abab34503013993cc9686ed99
pghazal <paul.ghazal@cegedim-srh.com>
2020-09-28 17:56:07 +0200

    Link to database + routes

---
+++ app/models/specs.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const SpecsSchema = mongoose.Schema({
+    title: String,
+    content: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Note', SpecsSchema);
